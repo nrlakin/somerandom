6050da76220250392ed8b0e5dcb5ad24d02ffbae
Neil Lakin <neil.lakin@gmail.com>
2015-12-08 17:49:59 -0500

    getting celery up

--- celery_config.py
+++ celery_config.py
@@ -1,6 +1,14 @@
 #!venv/bin/python
 from celery import Celery
+from datetime import timedelta
 
+CELERYBEAT_SCHEDULE = {
+    'update_followers': {
+        'task': 'tasks.follow_back',
+        'schedule' : timedelta(seconds=30),
+        'args' : ()
+    }
+}
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
@@ -12,4 +20,3 @@ def make_celery(app):
                 return TaskBase.__call__(self, *args, **kwargs)
         celery.Task = ContextTask
         return celery
-        
